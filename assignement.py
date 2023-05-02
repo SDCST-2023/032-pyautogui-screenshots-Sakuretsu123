a9fa18b0c0a8d56b6822f217cac6cb5f7964e979
Valerian <124203346+Sakuretsu123@users.noreply.github.com>
2023-05-02 11:28:11 -0700

    Update assignement.py

---
+++ assignement.py
@@ -0,0 +1,45 @@
+#python3 
+import pyautogui
+import time 
+
+'''
+sword merging simulator : https://www.crazygames.com/game/sword-merging-simulator
+command to plan :
+
+
+collect the orbs when 30/30 (click on the sword and left click to collect the orbs )
+merge sword if possible
+
+when orbs are not full, attack the monster by clicking on the skull 
+attack the monster by clicking on it,
+
+add sword at all time 
+
+
+after 10 mins, rebirth : 
+click on rebirth icon 
+accept rebirth 
+'''
+
+def monsters_hunt(): 
+    while #the screen thing is not 30/30 click on the monster
+    #+ if sword icon is bright click on it
+    rebirth()
+
+def swords_screen(): 
+    #collect the orbs
+    #if merge icon is bright click on it
+    #+ if sword icon is bright click on it
+    #after 3 seconds, go back to monster hunt
+
+    monsters_hunt()
+
+def rebirth(): 
+    #if time.time - x > 600 
+    #rebirth and follow instructions 
+
+
+    swords_screen()
+
+if __name__ == "__main__":
+    rebirth()
