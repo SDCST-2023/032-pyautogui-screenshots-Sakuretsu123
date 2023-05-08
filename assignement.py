49d5d60f0f2905f4e23a364ee102fdd4c0b8722b
Valerian <valerian210404@gmail.com>
2023-05-08 11:31:59 -0700

    done and working

--- assignement.py
+++ assignement.py
@@ -23,8 +23,8 @@ accept rebirth
 
 
 def swords_screen(): 
+    time.sleep(1)
     print('sword screen')
-    pyautogui.alert('sword screen')
     m,n = pyautogui.locateCenterOnScreen('mainscreen.PNG', confidence = 0.9)
     
 
@@ -32,7 +32,7 @@ def swords_screen():
     pyautogui.click()
     
     x, y = pyautogui.locateCenterOnScreen('bubble.PNG', confidence=0.8)
-    pyautogui.alert(f"{x} {y}")
+    
 
     pyautogui.moveTo(x-350,y)
     pyautogui.mouseDown()
@@ -40,7 +40,7 @@ def swords_screen():
     myList = [(x-190, y), (x-190, y+40), (x-500, y+40), (x-500, y)]
 
     for i in myList : 
-        pyautogui.moveTo(i, duration = 0.5)
+        pyautogui.moveTo(i, duration = 0.3)
         
 
     pyautogui.mouseUp()
@@ -49,10 +49,23 @@ def swords_screen():
     p, l = pyautogui.locateCenterOnScreen('buysword.PNG', confidence=0.9)
     
     if merge != None:
+        pyautogui.moveTo(p +50, l)
+
+        pyautogui.click()
+        time.sleep(0.1)
+        pyautogui.click()
+        pyautogui.click()
+        time.sleep(0.1)
+        pyautogui.click()
+        time.sleep(0.1)
+        pyautogui.click()
+        pyautogui.click()
+        pyautogui.click()
+        pyautogui.click()
+        pyautogui.click()
+
         pyautogui.moveTo(merge)
         pyautogui.click()
-        pyautogui.moveTo(p +50, l)
-        pyautogui.click(clicks = 5, duration= 1)
     
         
     
@@ -78,6 +91,7 @@ def monsters_hunt():
         if monster != None: 
             pyautogui.moveTo(x +100, y)
             pyautogui.click(clicks = 700)
+    time.sleep(1.2)
 
 
     rebirth()
@@ -88,27 +102,27 @@ def monsters_hunt():
     #if merge icon is bright click on it
     #(+ if sword icon is bright click on it)
     #(after 2 seconds, go back to monster hunt)
-
+def counting(): 
+    global count 
+    count = time.time()
     
 
 def rebirth(): 
-    i= 0
-    i = i + 1
-    if i == 1 :
-        count = time.time()
 
     print('rebirth')
     now = time.time()
-    if (count - now) > 5 : 
-        x = pyautogui.locateCenterOnScreen('rebirthbutton.PNG', confidence=0.8)
-        y = pyautogui.locateCenterOnScreen('rebirthtool.PNG', confidence=0.8)
-        z = pyautogui.locateCenterOnScreen('yesbutton.PNG', confidence=0.8)
-        pyautogui.moveTo(x)
+    if (now - count) > 120 : 
+        time.sleep(15)
+        x, y = pyautogui.locateCenterOnScreen('rebirthtool.PNG' , confidence=0.9)
+        pyautogui.moveTo(x, y)
         pyautogui.click()
-        pyautogui.moveTo(y)
+        l, m = pyautogui.locateCenterOnScreen('rebirthbutton.PNG',  confidence=0.9)
+        pyautogui.moveTo(l, m)
         pyautogui.click()
-        pyautogui.moveTo(z)
+        o, p = pyautogui.locateCenterOnScreen('yesbutton.PNG', confidence=0.9)
+        pyautogui.moveTo(o, p)
         pyautogui.click()
+        counting()
 
     #if time.time - x > 600 
     #rebirth and follow instructions 
@@ -118,7 +132,9 @@ def rebirth():
 
 
 if __name__ == "__main__":
+    counting()
     pyautogui.confirm('run?')
     
     while True:
         rebirth()
+        
